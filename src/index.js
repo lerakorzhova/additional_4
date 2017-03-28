61d84ed00416964d8c67d25c99dd1fb6fe01ba50
Korzhova Valeriya <lerakorzhova98@gmail.com>
2017-03-28 18:07:41 +0300

    product/initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    let resultArr = [];
+    let firstArr = first.split('').map(x => Number(x));
+    let secondArr = second.split('').map(x => Number(x));
+    resultArr.length = firstArr.length + secondArr.length;
+    resultArr.fill(0);
+
+    for (let i = secondArr.length - 1, k = 0, m = k; i >= 0; i--, k++) {
+        m = k;
+        for (let j = firstArr.length - 1; j >= 0; j--, m++) {
+            if (typeof firstArr[j] === 'undefined' || typeof secondArr[i] === 'undefined') continue;
+            resultArr[m] += firstArr[j] * secondArr[i];
+        }
+    }
+    let buffer = 0;
+    for (let i = 0; i < resultArr.length; i++) {
+        resultArr[i] += buffer;
+        buffer = Math.floor(resultArr[i] / 10);
+        resultArr[i] = resultArr[i] % 10;
+    }
+    resultArr = resultArr.reverse().join('').replace(/^0+/, '');
+    return resultArr;
 }
